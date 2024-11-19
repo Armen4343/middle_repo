6f1a4082807c15203a461987ae28079ea104f44e
Armen Petrosyan <109369539+Armen4343@users.noreply.github.com>
2024-11-19 15:32:00 +0400

    dd

--- .github/workflows/scripts/trigger_and_monitor_workflow.sh
+++ .github/workflows/scripts/trigger_and_monitor_workflow.sh
@@ -90,7 +90,7 @@ log "Workflow concluded with status: $conclusion"
 # Display all jobs with it's status and link
 github_api_call "GET" "/repos/${OWNER}/${REPO}/actions/runs/${wfid}/jobs" | jq -r '.jobs[] | "\(.name) - \(.status) - \(.conclusion) - \(.html_url)"'
 
-if [ $conclusion= "success" ]; then
+if [ "$conclusion" = "success" ]; then
     echo "Workflow run successful"
 else
     echo "Workflow run failed"
