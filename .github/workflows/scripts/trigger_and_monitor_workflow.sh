dd24ea17fb2d42a97915ac494103e9a0e89cfb54
Armen Petrosyan <109369539+Armen4343@users.noreply.github.com>
2024-11-27 17:42:48 +0400

    f

--- .github/workflows/scripts/trigger_and_monitor_workflow.sh
+++ .github/workflows/scripts/trigger_and_monitor_workflow.sh
@@ -42,26 +42,28 @@ WAIT_TIME=${SLEEP_TIME:-10}
 echo "${EVENT_TYPE}"
 echo "eveeeeeeeeeeeeeeeeeeent type"
 # Trigger the repository_dispatch event
-log "Triggering repository dispatch event in ${OWNER}/${REPO}..."
+log INFO "Triggering repository dispatch event in ${OWNER}/${REPO}..."
 resp=$(github_api_call "POST" "/repos/${OWNER}/${REPO}/dispatches" \
   "{\"event_type\": \"${EVENT_TYPE}\", \"client_payload\": {\"repository_name\": \"${CURRENT_REPO}\"}}")
+log INFO "Workflow dispatch event triggered successfully."
+
 # Check the trigger for errors
 if echo "$resp" | grep -q "message"; then
-    log "Error: ${resp}"
+    log ERROR "Error: ${resp}"
     exit 1
 else
-    log "Workflow triggered successfully"
+    log INFO "Workflow triggered successfully"
 fi
 
 # Find the triggered workflow run
-log "Waiting for workflow to start..."
+log INFO "Waiting for workflow to start..."
 counter=0
 while true; do
     counter=$((counter + 1))
     workflow=$(github_api_call "GET" "/repos/${OWNER}/${REPO}/actions/runs?event=repository_dispatch" | jq '.workflow_runs[0]')
     
     if [ -z "$workflow" ] || [ "$workflow" = "null" ]; then
-        log "No workflow runs found. Retrying..."
+        log ERROR "Workflow did not start within the allotted time of $MAX_TIME seconds."
         sleep 4
         # continue
     fi
@@ -71,10 +73,10 @@ while true; do
     tdif=$(( $(date -d "$atime" +"%s") - $(date -d "$wtime" +"%s") ))
     if [ "$tdif" -gt 10 ]; then
         if [ "$counter" -gt 3 ]; then
-            log "Workflow not found after multiple attempts"
+            log INFO "Workflow not found after multiple attempts"
             exit 1
         else
-            log "Waiting for workflow to start..."
+            log INFO "Waiting for workflow to start..."
             sleep 3
         fi
     else
@@ -84,32 +86,32 @@ done
 
 wfid=$(echo "$workflow" | jq -r '.id')
 conclusion=$(echo "$workflow" | jq -r '.conclusion')
-log "Workflow ID: ${wfid}"
+log INFO "Workflow ID: ${wfid}"
 
 # Wait for the workflow to complete
 counter=0
 while [ "$conclusion" = "null" ]; do
     if [ "$counter" -ge "$MAX_TIME" ]; then
-        log "Time limit exceeded"
+        log ERROR "Time limit exceeded"
         exit 1
     fi
     
-    log "Check run on https://github.com/${OWNER}/${REPO}/actions/runs/${wfid}"
+    log INFO "Check run on https://github.com/${OWNER}/${REPO}/actions/runs/${wfid}"
     sleep "$WAIT_TIME"
     
     conclusion=$(github_api_call "GET" "/repos/${OWNER}/${REPO}/actions/runs/${wfid}" | jq -r '.conclusion')
     counter=$((counter + WAIT_TIME))
 done
 
-log "Workflow concluded with status: $conclusion"
+log INFO "Workflow concluded with status: $conclusion"
 
 # Display all jobs with it's status and link
 github_api_call "GET" "/repos/${OWNER}/${REPO}/actions/runs/${wfid}/jobs" | jq -r '.jobs[] | "\(.name) - \(.status) - \(.conclusion) - \(.html_url)"'
 echo "workflow_conclusion=success" >> $GITHUB_ENV
 
 if [ "$conclusion" = "success" ]; then
-    log "Workflow run successful"
+    log INFO "Workflow run successful"
 else
-    log "Workflow run failed"
+    log ERROR "Workflow run failed"
     exit 1
 fi
